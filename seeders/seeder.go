d2e3e97193e9457b95a8396820be3287132d71ba
ipincamp <support@nur-arifin.my.id>
2025-08-01 10:28:55 +0700

    feat: Add user seeding functionality; implement seedUsers function to create default users

--- seeders/seeder.go
+++ seeders/seeder.go
@@ -5,6 +5,7 @@ import (
 	"ipincamp/srikandi-sehat/database"
 	"ipincamp/srikandi-sehat/src/constants"
 	"ipincamp/srikandi-sehat/src/models"
+	"ipincamp/srikandi-sehat/src/utils"
 	"log"
 )
 
@@ -16,6 +17,7 @@ func main() {
 
 	seedRoles()
 	seedPermissions()
+	seedUsers()
 
 	log.Println("Seeding completed successfully!")
 }
@@ -48,4 +50,62 @@ func seedPermissions() {
 		}
 	}
 	log.Println("Permissions seeded")
+}
+
+func seedUsers() {
+	type UserSeed struct {
+		Name     string
+		Email    string
+		Password string
+		RoleName constants.RoleName
+	}
+
+	usersToSeed := []UserSeed{
+		{
+			Name:     "Admin Name",
+			Email:    config.Get("ADMIN_EMAIL"),
+			Password: config.Get("USER_PASSWORD"),
+			RoleName: constants.AdminRole,
+		},
+		{
+			Name:     "User Name",
+			Email:    config.Get("USER_EMAIL"),
+			Password: config.Get("USER_PASSWORD"),
+			RoleName: constants.UserRole,
+		},
+	}
+
+	for _, userData := range usersToSeed {
+		if userData.Email == "" || userData.Password == "" {
+			log.Printf("Skipping seeding for role %s: email or password not set in .env", userData.RoleName)
+			continue
+		}
+
+		hashedPassword, err := utils.HashPassword(userData.Password)
+		if err != nil {
+			log.Fatalf("Could not hash password for %s: %v", userData.Email, err)
+		}
+
+		var role models.Role
+		if err := database.DB.First(&role, "name = ?", userData.RoleName).Error; err != nil {
+			log.Fatalf("Role '%s' not found. Please run role seeder first.", userData.RoleName)
+		}
+
+		user := models.User{
+			Name:     userData.Name,
+			Email:    userData.Email,
+			Password: hashedPassword,
+		}
+
+		var existingUser models.User
+		if err := database.DB.FirstOrCreate(&existingUser, models.User{Email: user.Email}, &user).Error; err != nil {
+			log.Fatalf("Could not seed user %s: %v", user.Email, err)
+		}
+
+		if err := database.DB.Model(&existingUser).Association("Roles").Replace(&role); err != nil {
+			log.Fatalf("Could not assign role to user %s: %v", user.Email, err)
+		}
+
+		log.Printf("User '%s' with role '%s' seeded successfully.", existingUser.Email, role.Name)
+	}
 }
